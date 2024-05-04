5fd53f4fb4490c5ec1f0dfc67471609200a93699
PCovered <dereckyao1006@gmail.com>
2024-05-05 03:22:47 +0800

    new

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-
+import MySlider from './MySlider';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="Name">
+          STUST <code>5B0G0009_姚映辰</code> upload
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn How Not to Fail This Class
         </a>
+
+        <section className="Work-react">
+
+          <div>
+            <h5>第1個元件</h5>
+            <MySlider />
+          </div>
+
+          <div>
+            <h5>第2個元件</h5>
+            <MySlider />
+          </div>
+
+          <div>
+            <h5>第3個元件</h5>
+            <MySlider />
+          </div>
+
+        </section>
+
       </header>
     </div>
   );
