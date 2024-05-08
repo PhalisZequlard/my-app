2915d468c15413adf07ef1787423c31be350a775
PCovered <dereckyao1006@gmail.com>
2024-05-09 01:03:20 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import MySlider from './MySlider';
+import "./App.css";
+import MyRGBPanel from "./components/MyRGBPanel";
+import MySlider from "./components/MySlider";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="Name">
-          STUST <code>5B0G0009_姚映辰</code> upload
-        </p>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn How Not to Fail This Class
-        </a>
-
-        <section className="Work-react">
-
-          <div>
-            <h5>第1個元件</h5>
-            <MySlider />
-          </div>
-
-          <div>
-            <h5>第2個元件</h5>
-            <MySlider />
-          </div>
-
-          <div>
-            <h5>第3個元件</h5>
-            <MySlider />
-          </div>
-
-        </section>
-
-      </header>
+      <h1>MySlider</h1>
+      <MySlider />
+      <h1>MyRGBPanel</h1>
+      <MyRGBPanel />
     </div>
   );
 }
