ec43af3c32135359cdac9e849832fdb8b91f52f6
PCovered <dereckyao1006@gmail.com>
2024-05-23 12:00:02 +0800

    update-8

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
 import MyRGBPanel from "./components/MyRGBPanel";
 import MySlider from "./components/MySlider";
+import MyCalculator from "./components/MyCalculator";
 
 function App() {
   return (
     <div className="App">
-      <h1>MySlider</h1>
-      <MySlider />
-      <h1>MyRGBPanel</h1>
-      <MyRGBPanel />
+      <div>
+        <h1>MySlider</h1>
+        <MySlider />
+        <h1>MyRGBPanel</h1>
+        <MyRGBPanel />
+      </div>
+      <div>
+        <h1>MyCalculator</h1>
+        <MyCalculator />
+      </div>
     </div>
   );
 }
